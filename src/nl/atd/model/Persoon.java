b43f8a9b0fd49918e6ef9f7d1472990f7076f4ad
Tom Valk <tomvalk@gmail.com>
2015-04-28 15:06:36 +0200

    Modelklasse persoon toegevoegd

--- src/nl/atd/model/Persoon.java
+++ src/nl/atd/model/Persoon.java
@@ -1,5 +1,30 @@
 package nl.atd.model;
 
+/**
+ * Generiek persoon klasse
+ * @author ATD Developers
+ *
+ */
 public abstract class Persoon {
-
+	private String naam;
+	
+	public Persoon(String nm) {
+		this.naam = nm;
+	}
+	
+	/**
+	 * Get Naam
+	 * @return naam van persoon
+	 */
+	public String getNaam() {
+		return this.naam;
+	}
+	
+	/**
+	 * Set Naam
+	 * @param nm naam van persoon
+	 */
+	public void setNaam(String nm) {
+		this.naam = nm;
+	}
 }
