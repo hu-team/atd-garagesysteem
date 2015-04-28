09a552d9e5f2a1aa09782ab0731f24d617d54f91
Tom Valk <tomvalk@gmail.com>
2015-04-28 15:42:02 +0200

    Klant klasse toegevoegd

---
+++ src/nl/atd/model/Klant.java
@@ -0,0 +1,65 @@
+package nl.atd.model;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+/**
+ * Klant object
+ * @author ATD Developers
+ *
+ */
+public class Klant extends Persoon {
+	private String email;
+	private Calendar laatsteBezoek;
+	
+	private ArrayList<Auto> autos;
+	
+	public Klant(String nm) {
+		super(nm);
+		
+		this.email = "";
+		this.laatsteBezoek = Calendar.getInstance();
+		this.autos = new ArrayList<Auto>();
+	}
+	
+	/**
+	 * Get alle auto's in arraylist
+	 * @return ArrayList met auto's
+	 */
+	public ArrayList<Auto> getAutos() {
+		return this.autos;
+	}
+	
+	/**
+	 * Voeg auto toe aan auto's
+	 * @param a Auto om toe te voegen
+	 */
+	public void voegAutoToe(Auto a) {
+		this.autos.add(a);
+	}
+	
+	/**
+	 * Get e-mail adres van klant
+	 * @return E-mail adres
+	 */
+	public String getEmail() {
+		return this.email;
+	}
+	
+	/**
+	 * Get laatste bezoek
+	 * @return laatste bezoek klant
+	 */
+	public Calendar getLaatsteBezoek() {
+		return this.laatsteBezoek;
+	}
+	
+	/**
+	 * Set laatste bezoek van klant
+	 * @param lb Laatste bezoek
+	 */
+	public void setLaatsteBezoek(Calendar lb) {
+		this.laatsteBezoek = lb;
+	}
+	
+}
