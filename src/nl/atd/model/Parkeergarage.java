343b727e07672ad40b56c3296495e4b289e10591
Tom Valk <tomvalk@gmail.com>
2015-04-29 13:02:51 +0200

    Vrije plek zoeken en javadoc aan parkeergarage toegevoegd

--- src/nl/atd/model/Parkeergarage.java
+++ src/nl/atd/model/Parkeergarage.java
@@ -3,6 +3,11 @@ package nl.atd.model;
 import java.util.ArrayList;
 import java.util.Calendar;
 
+/**
+ * Parkeergarage zorgt voor een dataopslag voor de plekken die in de garage zitten.
+ * @author ATD Developers
+ *
+ */
 public class Parkeergarage {
 
 	private static Parkeergarage instance;
@@ -12,6 +17,10 @@ public class Parkeergarage {
 		this.plekken = new ArrayList<Parkeerplek>();
 	}
 	
+	/**
+	 * Get instantie van parkeergarage.
+	 * @return
+	 */
 	public static Parkeergarage getInstance(){
 		if(instance == null){
 			instance = new Parkeergarage();
@@ -19,11 +28,27 @@ public class Parkeergarage {
 		return instance;
 	}
 	
+	/**
+	 * Get alle plekken
+	 * @return plekken
+	 */
 	public ArrayList<Parkeerplek> getAllePlekken(){
 		return this.plekken;
 	}
 	
+	/**
+	 * Zoek vrije plek in garage, return de vrije plek, returnt null als er geen plek is.
+	 * 
+	 * @param van vanaf welke datum en tijd?
+	 * @param tot tot welke datum en tijd?
+	 * @return de plek of null als er geen vrije plek is
+	 */
 	public Parkeerplek zoekVrijePlek(Calendar van, Calendar tot){
+		for(int i = 0; i < this.plekken.size(); i++) {
+			if(this.plekken.get(i).isVrijTussen(van, tot)) {
+				return this.plekken.get(i);
+			}
+		}
 		
 		return null;
 	}
