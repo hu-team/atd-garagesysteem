c065b79fd6f0540c950ecaba045b7fcfa1687e5a
ArjanShoj <arjan.shojaeian@gmail.com>
2015-04-29 13:32:46 +0200

    Parkeerplek en Reservering aangepast. Methodes afgemaakt.

--- src/nl/atd/model/Parkeerplek.java
+++ src/nl/atd/model/Parkeerplek.java
@@ -3,6 +3,12 @@ package nl.atd.model;
 import java.util.ArrayList;
 import java.util.Calendar;
 
+/**
+ * 
+ * @author ATD Developer
+ *
+ */
+
 public class Parkeerplek {
 	
 	private char rij;
@@ -15,28 +21,73 @@ public class Parkeerplek {
 		this.plek = plek;
 	}
 	
+	/**
+	 * Get rij
+	 * @return char rij
+	 */
 	public char getRij(){
 		return this.rij;
 	}
 	
+	/**
+	 * Get plek
+	 * @return int plek
+	 */
 	public int getPlek(){
 		return this.plek;
 	}
 	
+	/**
+	 * Is bezet vandaag?
+	 * @return boolean 
+	 */
 	public boolean isBezet(){
-		
-		return false;
+		return !this.isVrijTussen(Calendar.getInstance(), Calendar.getInstance());
 	}
 	
+	/**
+	 * Voegt een reservering bij
+	 * @param Reservering res
+	 */
 	public void addReservering(Reservering res){
-		
+		reserveringen.add(res);
 	}
 	
+	/**
+	 * Get alle reserveringen
+	 * @return ArrayList<Reservering> reservering
+	 */
 	public ArrayList<Reservering> getAlleReserveringen(){
 		return this.reserveringen;
 	}
 	
+	/**
+	 * Is vrij tussen bepaalde datum?
+	 * @param begin datum van reservering
+	 * @param eind datum van reservering
+	 * @return boolean
+	 */
 	public boolean isVrijTussen(Calendar begin, Calendar eind){
-		return false;
+		for(Reservering res : reserveringen){
+			if(begin.after(res.getVan()) && begin.before(res.getTot())  
+					|| eind.after(res.getVan()) && eind.before(res.getTot())){
+				return false;
+			}
+		}		
+		return true;
+	}
+	
+	/**
+	 * Get huidige reservering
+	 * @return Reservering res
+	 */
+	public Reservering getHuidigeReservering(){
+		for(Reservering res : reserveringen){
+			if(Calendar.getInstance().after(res.getVan()) 
+					&& Calendar.getInstance().before(res.getTot())){
+				return res;
+			}
+		}
+		return null;
 	}
 }
--- src/nl/atd/model/Reservering.java
+++ src/nl/atd/model/Reservering.java
@@ -1,5 +1,97 @@
 package nl.atd.model;
 
-public class Reservering {
+import java.util.Calendar;
 
+/**
+ * 
+ * @author ATD Developer
+ *
+ */
+
+public class Reservering extends FactuurOnderdeel{
+	private Calendar van;
+	private Calendar tot;
+	
+	private Klant klant;
+	private Auto auto;
+	
+	public Reservering(Klant kl, Auto au){
+		this.klant = kl;
+		this.auto = au;
+		
+		van = Calendar.getInstance();
+		tot = Calendar.getInstance();
+	}
+	
+	/**
+	 * Set begin datum van reservering
+	 * @param vn van
+	 */
+	public void setVan(Calendar vn){
+		van = vn;
+	}
+	
+	/**
+	 * Set eind datum van reservering
+	 * @param tt tot
+	 */
+	public void setTot(Calendar tt){
+		tot = tt;
+	}
+	
+	/**
+	 * Get begin datum
+	 * @return Calendar van
+	 */
+	public Calendar getVan(){
+		return van;
+	}
+	
+	/**
+	 * Get eind datum
+	 * @return Calendar tot
+	 */
+	public Calendar getTot(){
+		return tot;
+	}
+	
+	/**
+	 * Get klant
+	 * @return Klant klant
+	 */
+	@Override
+	public Klant getKlant(){
+		return klant;
+	}
+	
+	/**
+	 * Get auto
+	 * @return Auto auto
+	 */
+	public Auto getAuto(){
+		return auto;
+	}
+
+	/**
+	 * Get totaal prijs
+	 * @return double totaal prijs
+	 */
+	@Override
+	public double getTotaalPrijs() {
+		
+		return 0;
+	}
+
+	/**
+	 * Get factuur omschrijving
+	 * @return String factooromschrijving
+	 */
+	@Override
+	public String getFactuurOmschrijving() {
+		
+		return null;
+	}
+	
+	
+	
 }
