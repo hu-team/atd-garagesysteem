12b4b3118b8b5bd8d419f4603f901acef4dc32f7
Tom Valk <tomvalk@gmail.com>
2015-05-09 16:32:15 +0200

    Login Servlet, Login url mapping /login

---
+++ src/nl/atd/controller/LoginServlet.java
@@ -0,0 +1,17 @@
+package nl.atd.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet {
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+	}
+}
