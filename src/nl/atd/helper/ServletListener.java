bc123e1d6374e610775382d2bac98a08a48f1f49
ArjanShoj <arjan.shojaeian@gmail.com>
2015-05-22 02:10:04 +0200

    Imports verwijderd.

--- src/nl/atd/helper/ServletListener.java
+++ src/nl/atd/helper/ServletListener.java
@@ -1,8 +1,6 @@
 package nl.atd.helper;
 
-import java.io.IOException;
 import java.util.logging.ConsoleHandler;
-import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletContextEvent;
