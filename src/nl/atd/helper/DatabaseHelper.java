f449971ccbdd39c0feaab49a23b9ad59118476c4
Tom Valk <tomvalk@gmail.com>
2015-05-27 14:43:45 +0200

    Database settings voor productieserver in comments gezet

--- src/nl/atd/helper/DatabaseHelper.java
+++ src/nl/atd/helper/DatabaseHelper.java
@@ -9,6 +9,11 @@ public class DatabaseHelper {
 	private static final String DB_PASS		= "";
 	private static final String DB_DRIVER	= "com.mysql.jdbc.Driver";
 	
+	// Productie server
+	// Gebuikersnaam: atd
+	// Wachtwoord: hvxDS5UfyQvZ7GZm
+	// Database: atd
+	
 	public static Connection getDatabaseConnection() throws Exception {		
 		Class.forName(DB_DRIVER).newInstance();
 		return DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
