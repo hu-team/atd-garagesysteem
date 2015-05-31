115839d12f5d3449f6955bbdee5e2aca424c891e
Max van Kuik <kuikvanmax@gmail.com>
2015-05-31 13:36:26 +0200

    junit-4.12.jar toegevoegd aan Build Path Test folder aangemaakt

---
+++ test/nl/atd/test/dao/KlantDAOTest.java
@@ -0,0 +1,35 @@
+package nl.atd.test.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class KlantDAOTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+	
+
+}
