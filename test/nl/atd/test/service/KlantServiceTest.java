821f7c28e4fdaf6cff92a1c46301b44f06160a0b
Max van Kuik <kuikvanmax@gmail.com>
2015-06-01 15:23:45 +0200

    Verwijderd: KlantDAOTest Vernieuwd: KlantServiceTest

--- test/nl/atd/test/service/KlantServiceTest.java
+++ test/nl/atd/test/service/KlantServiceTest.java
@@ -14,7 +14,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-
 public class KlantServiceTest {
 	KlantService kservice = ServiceProvider.getKlantService();
 	private Klant k1, k2, k3, k4;
@@ -24,13 +23,12 @@ public class KlantServiceTest {
 
 	@Before
 	public void setUp() throws Exception {
-		// setUp voor testGetAlleKlanten()
 		k1 = new Klant("Max van Kuik");
 		k1.setEmail("kuikvanmax@hotmail.com");
 		k1.setGebruikersnaam("maxiiemaxx");
 		k1.setWachtwoord(AuthHelper.encryptWachtwoord("123"));
 		k1.setLaatsteBezoek(null);
-	//	kservice.addKlant(k1);
+		// kservice.addKlant(k1);
 		klanten.add(k1);
 
 		k2 = new Klant("Tom Valk");
@@ -38,29 +36,37 @@ public class KlantServiceTest {
 		k2.setGebruikersnaam("tomvalk");
 		k2.setWachtwoord(AuthHelper.encryptWachtwoord("456"));
 		k2.setLaatsteBezoek(null);
-	//	kservice.addKlant(k2);
+		// kservice.addKlant(k2);
 		klanten.add(k2);
-		
+
 		actualKlanten = kservice.getAlleKlanten();
-		k3 = actualKlanten.get(0);
-		k4 = actualKlanten.get(1);
-		
-		// setUp voor testGetKlantByGebruikersnaam()
+
 	}
 
 	@Test
 	public void testGetAlleKlanten() {
-		// Klant k1 ( aangemaakt in de setUp() ) zou gelijk moeten zijn aan Klant k3 ( Uit Database gehaald ) 
-		// Klant k2 ( aangemaakt in de setUp() ) zou gelijk moeten zijn aan Klant k4 ( Uit Database gehaald )
+		k3 = actualKlanten.get(0);
+		k4 = actualKlanten.get(1);
+
+		// Klant k1 ( aangemaakt in de setUp() ) zou gelijk moeten zijn aan
+		// Klant k3 ( Uit Database gehaald )
+		// Klant k2 ( aangemaakt in de setUp() ) zou gelijk moeten zijn aan
+		// Klant k4 ( Uit Database gehaald )
 
 		assertEquals(k1, k3);
 		assertEquals(k2, k4);
 	}
-	
+
 	@Test
 	public void testGetKlantByGebruikersnaam() {
-		
+		k3 = kservice.getKlantByGebruikersnaam("maxiiemaxx");
+		k4 = kservice.getKlantByGebruikersnaam("tomvalk")
+
+		assertEquals("k1 zou gelijk moeten zijn aan k3", k1, k3);
 	}
+	
+	
+
 
 	@After
 	public void tearDown() throws Exception {
@@ -68,10 +74,11 @@ public class KlantServiceTest {
 		k2 = null;
 		k3 = null;
 		k4 = null;
-		
+
 		klanten = null;
 		actualKlanten = null;
-	}
 
+		// k1 & k2, verwijderen uit database ( of database legen)
+	}
 
 }
