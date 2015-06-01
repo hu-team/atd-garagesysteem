36dbb917469af3a126e0438e9113d65a9701b758
ArjanShoj <arjan.shojaeian@gmail.com>
2015-06-01 14:52:08 +0200

    AddKlusServlet aangemaakt en KlusDAO aangepast.

--- src/nl/atd/service/KlusService.java
+++ src/nl/atd/service/KlusService.java
@@ -3,7 +3,10 @@ package nl.atd.service;
 import java.util.ArrayList;
 
 import nl.atd.dao.KlusDAO;
+import nl.atd.model.Auto;
+import nl.atd.model.Klant;
 import nl.atd.model.Klus;
+import nl.atd.model.Monteur;
 
 public class KlusService {
 	private KlusDAO klusDAO = new KlusDAO();
@@ -14,6 +17,10 @@ public class KlusService {
 	 */
 	public ArrayList<Klus> getKlussen() {
 		return this.klusDAO.getAlleKlussen();
+	}
+	
+	public boolean addKlus(Klus klus, Auto auto, Monteur monteur, Klant klant){
+		return this.klusDAO.addKlus(klus, auto, monteur, klant);
 	}
 	
 }
