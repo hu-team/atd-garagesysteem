2ff1dc93b107597c6d4121962155fa946cb54286
Max van Kuik <kuikvanmax@gmail.com>
2015-06-02 02:31:20 +0200

    Tests toegevoegd

--- test/nl/atd/test/service/AutoServiceTest.java
+++ test/nl/atd/test/service/AutoServiceTest.java
@@ -57,8 +57,8 @@ public class AutoServiceTest {
 
 	@Test
 	public void testGetAlleAutos() {
-		a3 = aservice.getAlleAutos().get(0);
-		a4 = aservice.getAlleAutos().get(1);
+		a5 = aservice.getAlleAutos().get(0);
+		a6 = aservice.getAlleAutos().get(1);
 
 		// Auto a1 ( aangemaakt in de setUp() ) zou gelijk moeten zijn aan
 		// Auto a3 ( Uit Database gehaald )
@@ -66,17 +66,23 @@ public class AutoServiceTest {
 		// Auto a2 ( aangemaakt in de setUp() ) zou gelijk moeten zijn aan
 		// Auto a4 ( Uit Database gehaald )
 
-		assertEquals(a1, a3);
-		assertEquals(a2, a4);
+		assertEquals(a1, a5);
+		assertEquals(a2, a6);
+
+		// Check of als het niet klopt, het ook word gezien
+
+		assertFalse(a1.equals(a6));
+		assertFalse(a1.equals(a2));
+		assertFalse(a5.equals(a6));
 	}
 
 	@Test
 	public void testGetAutosVanKlant() {
-		a5 = aservice.getAutosVanKlant("maxiiemaxx").get(0);
-		a6 = aservice.getAutosVanKlant("maxiiemaxx").get(1);
+		a5 = aservice.getAutosVanKlant("maxiiemaxx").get(0); // Mercedes A180
+		a6 = aservice.getAutosVanKlant("maxiiemaxx").get(1); // Volvo V60
 
-		a7 = aservice.getAutosVanKlant("tomvalk").get(0);
-		a8 = aservice.getAutosVanKlant("tomvalk").get(1);
+		a7 = aservice.getAutosVanKlant("tomvalk").get(0); // Ford Mondeo
+		a8 = aservice.getAutosVanKlant("tomvalk").get(1); // Volkswagen Golf
 
 		// Auto a1 ( aangemaakt in de setUp() ) zou gelijk moeten zijn aan
 		// Auto a5 ( Uit Database gehaald )
@@ -85,16 +91,54 @@ public class AutoServiceTest {
 
 		assertEquals(a1, a5);
 		assertEquals(a3, a6);
-		
+
 		// Auto a2 ( aangemaakt in de setUp() ) zou gelijk moeten zijn aan
-		// Auto a7 ( Uit Database gehaald )
+		// Auto a7 ( Uit Database gehaald d.m.v. de gebruikersnaam )
 		// Auto a4 ( aangemaakt in de setUp() ) zou gelijk moeten zijn aan
-		// Auto a8 ( Uit Database gehaald )
-		
+		// Auto a8 ( Uit Database gehaald d.m.v. de gebruikersnaam )
+
 		assertEquals(a2, a7);
 		assertEquals(a4, a8);
 	}
-	
+
+	@Test(expected = Exception.class)
+	public void testGetAutosVanKlant1() {
+		// John bestaat niet
+		// En heeft dus geen autos, dus een Exception
+		a5 = aservice.getAutosVanKlant("John").get(0);
+	}
+
+	@Test
+	public void testGetAutoOpkenteken() {
+		a5 = aservice.getAutoOpKenteken("GB1231"); // Mercedes A180
+
+		// Auto a1 ( aangemaakt in de setUp() ) zou gelijk moeten zijn aan
+		// Auto a5 ( Uit Database gehaald d.m.v. het kenteken )
+
+		assertEquals(a1, a5);
+
+		// Als kenteken niet bestaat
+		// Wordt null gereturned, dus a6 zou null moeten zijn
+		a6 = aservice.getAutoOpKenteken("GG9991");
+
+		assertTrue(a6 == null);
+	}
+
+	// Voordat deze gemaakt kan worden moet eerst alle autos worden verwijderd
+	// bij tearDown() uit database
+
+	// @Test
+	// public void testGetAuto() {
+	// aservice.getAuto(1); // get auto met id 1
+	// }
+
+	// Voordat deze gemaakt kan worden moet eerst alle autos worden verwijderd
+	// bij tearDown() uit database
+
+	// @Test
+	// public void testGetAutoIdOpKenteken() {
+	// aservice.getAutoIdOpKenteken("GB1231"); // get autoid met van Mercedes A180
+	// }
 
 	@After
 	public void tearDown() throws Exception {
