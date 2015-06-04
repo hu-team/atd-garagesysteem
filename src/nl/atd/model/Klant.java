e4197528bebee8e5970ce7aa168e2b6021cd21d5
Tom Valk <tomvalk@gmail.com>
2015-06-04 23:09:22 +0200

    Equals klant gefixt, null check

--- src/nl/atd/model/Klant.java
+++ src/nl/atd/model/Klant.java
@@ -97,13 +97,11 @@ public class Klant extends Persoon {
 		// Omdat in de tests de database moet worden geleegd bij tearDown()
 		//b = b && (this.autos.equals(((Klant)ander).getAutos()));
 		
-		
 		// Calendar vergelijken, DAY.OF.YEAR - YEAR - HOURS : MINUTES
-		
-	//	b = b && ((this.laatsteBezoek).get(Calendar.DAY_OF_YEAR) == ((Klant)ander).getLaatsteBezoek().get(Calendar.DAY_OF_YEAR));
-		b = b && ((this.laatsteBezoek).get(Calendar.YEAR) == ((Klant)ander).getLaatsteBezoek().get(Calendar.YEAR));
-		b = b && ((this.laatsteBezoek).get(Calendar.HOUR_OF_DAY) == ((Klant)ander).getLaatsteBezoek().get(Calendar.HOUR_OF_DAY));
-		b = b && ((this.laatsteBezoek).get(Calendar.MINUTE) == ((Klant)ander).getLaatsteBezoek().get(Calendar.MINUTE));
+		b = b && ((this.laatsteBezoek == null && ((Klant)ander).getLaatsteBezoek() == null) || 
+				((this.laatsteBezoek).get(Calendar.YEAR) == ((Klant)ander).getLaatsteBezoek().get(Calendar.YEAR) &&
+				(this.laatsteBezoek).get(Calendar.HOUR_OF_DAY) == ((Klant)ander).getLaatsteBezoek().get(Calendar.HOUR_OF_DAY) &&
+				(this.laatsteBezoek).get(Calendar.MINUTE) == ((Klant)ander).getLaatsteBezoek().get(Calendar.MINUTE)));
 		
 		return b;
 	}
