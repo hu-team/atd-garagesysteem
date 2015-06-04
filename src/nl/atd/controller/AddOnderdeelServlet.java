fedb88b1c42de35bae537698bda58790d8d0ac76
ArjanShoj <arjan.shojaeian@gmail.com>
2015-06-04 15:45:55 +0200

    OnderdeelDAO en OnderdeelService aangepast. addOnderdeel methode toegevoegd. AddOnderdeelServlet alvast aangemaakt(Niet klaar).

---
+++ src/nl/atd/controller/AddOnderdeelServlet.java
@@ -0,0 +1,18 @@
+package nl.atd.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddOnderdeelServlet extends HttpServlet{
+	private static final long serialVersionUID = 2216519323795325903L;
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+	}
+	
+}
