647cce422dd59884d055bad01d815473c40b26a3
Max van Kuik <kuikvanmax@gmail.com>
2015-06-15 23:17:01 +0200

    KlantTest aangemaakt

---
+++ test/nl/atd/test/model/KlantTest.java
@@ -0,0 +1,24 @@
+package nl.atd.test.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class KlantTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
