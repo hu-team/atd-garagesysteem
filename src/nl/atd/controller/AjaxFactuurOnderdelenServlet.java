376bbeb72385db563dd482536dad594f206d20e9
ArjanShoj <arjan.shojaeian@gmail.com>
2015-06-21 17:13:49 +0200

    no message

---
+++ src/nl/atd/controller/AjaxFactuurOnderdelenServlet.java
@@ -0,0 +1,31 @@
+package nl.atd.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AjaxFactuurOnderdelenServlet
+ */
+@WebServlet("/AjaxFactuurOnderdelenServlet")
+public class AjaxFactuurOnderdelenServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String klant = request.getParameter("klant");
+	}
+
+}
